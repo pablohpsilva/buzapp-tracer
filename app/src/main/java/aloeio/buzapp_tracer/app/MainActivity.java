ddf6577fa1ad261906925aff536bc61ea4a93ad6
pablohenrique <pablo.haoko@gmail.com>
2015-09-07 19:26:06 -0300

    fixed bugs

--- app/src/main/java/aloeio/buzapp_tracer/app/MainActivity.java
+++ app/src/main/java/aloeio/buzapp_tracer/app/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends FragmentActivity implements
                             }
                         }
                     }).start();
-                    
+
                 }
 
             }
